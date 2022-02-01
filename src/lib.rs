339f915123e30c136f95b88556c3a93534d5d883
Abner Kaizer <abnerkaizer@protonmail.com>
2022-02-01 09:58:06 -0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub fn lix(a: usize, b: usize, c: usize) -> usize {
+    let lix_return: usize = a / b + (c * 100) / a;
+    lix_return
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = lix(400_000, 2_500, 50_000);
+        assert_eq!(result, 172);
+    }
+}
